6bb9e7b5f3471c4c0eb1cb008ae22d6b5b3e0d65
tanyuan <tanyuan@zallds.com>
2017-07-10 19:14:52 +0800

    大文件上传  处理文件以及回调credit处理

--- modules/core-mybatis/src/main/java/com/thoughtworks/fms/core/mybatis/util/BatchUploadUtil.java
+++ modules/core-mybatis/src/main/java/com/thoughtworks/fms/core/mybatis/util/BatchUploadUtil.java
@@ -4,90 +4,84 @@ package com.thoughtworks.fms.core.mybatis.util;
  * Created by TanYuan on 2017/6/30.
  */
 
-import com.thoughtworks.fms.core.FileRepository;
 import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 import org.slf4j.LoggerFactory;
 
-import javax.inject.Inject;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import java.io.*;
 
-    /**
-     * Servlet implementation class PluploadServlet
-     */
+/**
+ * Servlet implementation class PluploadServlet
+ */
 //    @WebServlet("/uploadFileForCreditForBatchUpload")
-    public class  BatchUploadUtil  extends HttpServlet {
-        private static final String BIGFILE_SERVERS = PropertiesLoader.getProperty("bigfile.servers");
+public class BatchUploadUtil extends HttpServlet {
+    private static final String BIGFILE_SERVERS = PropertiesLoader.getProperty("bigfile.servers");
 
-        private static final String BIGFILE_HANDLE_SCRIPT = PropertiesLoader.getProperty("bigfile.handle.script");
+    private static final String BIGFILE_HANDLE_SCRIPT = PropertiesLoader.getProperty("bigfile.handle.script");
 
-        private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-        private static final int BUFFER_SIZE = 100 * 1024;
+    private static final int BUFFER_SIZE = 100 * 1024;
 
-        private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(BatchUploadUtil.class);
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(BatchUploadUtil.class);
 
 
 //        public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //            doPost(request, response);
 //        }
 
-        public static int getBatchUpload(FormDataMultiPart multiPart,InputStream fileInputStream, HttpServletRequest request) {
-            try {
+    public static int getBatchUpload(FormDataMultiPart multiPart, InputStream fileInputStream, HttpServletRequest request) {
+        try {
 //                String name = request.getParameter("name");
-                int res;
-                String name = multiPart.getField("name").getValueAs(String.class);
-                Integer chunk = 0, chunks = 0;
-                chunk= multiPart.getField("chunk").getValueAs(Integer.class);
-                chunks=multiPart.getField("chunks").getValueAs(Integer.class);
-                if(null != request.getParameter("chunk") && !request.getParameter("chunk").equals("")){
-                    chunk = Integer.valueOf(request.getParameter("chunk"));
-                }
-                if(null != request.getParameter("chunks") && !request.getParameter("chunks").equals("")){
-                    chunks = Integer.valueOf(request.getParameter("chunks"));
-                }
-                LOGGER.info("chunk:[" + chunk + "] chunks:[" + chunks + "]");
-                //检查文件目录，不存在则创建
-                //String relativePath = "/plupload/files/";
-                //String realPath = request.getServletContext().getRealPath("");
+            int res;
+            String name = multiPart.getField("name").getValueAs(String.class);
+            Integer chunk = 0, chunks = 0;
+            chunk = multiPart.getField("chunk").getValueAs(Integer.class);
+            chunks = multiPart.getField("chunks").getValueAs(Integer.class);
+            if (null != request.getParameter("chunk") && !request.getParameter("chunk").equals("")) {
+                chunk = Integer.valueOf(request.getParameter("chunk"));
+            }
+            if (null != request.getParameter("chunks") && !request.getParameter("chunks").equals("")) {
+                chunks = Integer.valueOf(request.getParameter("chunks"));
+            }
+            LOGGER.info("chunk:[" + chunk + "] chunks:[" + chunks + "]");
+            //检查文件目录，不存在则创建
+            //String relativePath = "/plupload/files/";
+            //String realPath = request.getServletContext().getRealPath("");
 //                File folder = new File(realPath + relativePath);
 //                if (!folder.exists()) {
 //                    folder.mkdirs();
 //                }
 
-                //目标文件
-                File destFile = new File(BIGFILE_SERVERS, name);
-                //文件已存在删除旧文件（上传了同名的文件）
-                if (chunk == 0 && destFile.exists()) {
-                    destFile.delete();
-                    destFile = new File(BIGFILE_SERVERS, name);
-                }
-                //合成文件
-                appendFile(fileInputStream, destFile);
-                if (chunk == chunks - 1) {
-                    LOGGER.info("上传完成");
-                    LOGGER.info("上传完成后调用服务器脚本进行文件处理");
-                    String startHandleBigFile = BIGFILE_HANDLE_SCRIPT + name;
-                    LOGGER.info("获取服务器大文件处理脚本命令:" + BIGFILE_HANDLE_SCRIPT);
-//                    try {
-//                        Process pro = Runtime.getRuntime().exec(startHandleBigFile);
-//                    } catch (IOException e) {
-//                        e.printStackTrace();
-//                    }finally {
-
-//                    }
-                return  1;
-
-                }else {
-                    LOGGER.info("还剩["+(chunks-1-chunk)+"]个块文件");
-                }
-            } catch (Exception e) {
-                LOGGER.error(e.getMessage());
+            //目标文件
+            File destFile = new File(BIGFILE_SERVERS, name);
+            //文件已存在删除旧文件（上传了同名的文件）
+            if (chunk == 0 && destFile.exists()) {
+                destFile.delete();
+                destFile = new File(BIGFILE_SERVERS, name);
             }
-            return 0;
+            //合成文件
+            appendFile(fileInputStream, destFile);
+            if (chunk == chunks - 1) {
+                LOGGER.info("上传完成");
+                LOGGER.info("上传完成后调用服务器脚本进行文件处理");
+                String startHandleBigFile = BIGFILE_HANDLE_SCRIPT + name;
+                LOGGER.info("获取服务器大文件处理脚本命令:" + BIGFILE_HANDLE_SCRIPT);
 
+                Process pro = Runtime.getRuntime().exec(startHandleBigFile);
+
+                return 1;
+
+            } else {
+                LOGGER.info("还剩[" + (chunks - 1 - chunk) + "]个块文件");
+            }
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
         }
+        return 0;
+
+    }
 //        public static void getBatchUpload(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
 //            try {
 //                boolean isMultipart = ServletFileUpload.isMultipartContent(request);
@@ -164,36 +158,36 @@ import java.io.*;
 //            }
 //        }
 
-        private static void appendFile(InputStream in, File destFile) {
-            OutputStream out = null;
-            try {
-                // plupload 配置了chunk的时候新上传的文件append到文件末尾
-                if (destFile.exists()) {
-                    out = new BufferedOutputStream(new FileOutputStream(destFile, true), BUFFER_SIZE);
-                } else {
-                    out = new BufferedOutputStream(new FileOutputStream(destFile),BUFFER_SIZE);
-                }
-                in = new BufferedInputStream(in, BUFFER_SIZE);
+    private static void appendFile(InputStream in, File destFile) {
+        OutputStream out = null;
+        try {
+            // plupload 配置了chunk的时候新上传的文件append到文件末尾
+            if (destFile.exists()) {
+                out = new BufferedOutputStream(new FileOutputStream(destFile, true), BUFFER_SIZE);
+            } else {
+                out = new BufferedOutputStream(new FileOutputStream(destFile), BUFFER_SIZE);
+            }
+            in = new BufferedInputStream(in, BUFFER_SIZE);
 
-                int len = 0;
-                byte[] buffer = new byte[BUFFER_SIZE];
-                while ((len = in.read(buffer)) > 0) {
-                    out.write(buffer, 0, len);
+            int len = 0;
+            byte[] buffer = new byte[BUFFER_SIZE];
+            while ((len = in.read(buffer)) > 0) {
+                out.write(buffer, 0, len);
+            }
+        } catch (Exception e) {
+            LOGGER.error(e.getMessage());
+        } finally {
+            try {
+                if (null != in) {
+                    in.close();
                 }
-            } catch (Exception e) {
+                if (null != out) {
+                    out.close();
+                }
+            } catch (IOException e) {
                 LOGGER.error(e.getMessage());
-            } finally {
-                try {
-                    if (null != in) {
-                        in.close();
-                    }
-                    if(null != out){
-                        out.close();
-                    }
-                } catch (IOException e) {
-                    LOGGER.error(e.getMessage());
-                }
             }
         }
+    }
 
 }
