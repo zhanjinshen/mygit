e480f7242d6cbea91e9812c5508665a08c06c391
hbutggl <geguanglei@zallds.com>
2018-01-17 14:22:50 +0800

    添加pdf文件转换筛选类型--大小写

--- modules/core/src/main/java/com/thoughtworks/fms/api/resources/FilesResource.java
+++ modules/core/src/main/java/com/thoughtworks/fms/api/resources/FilesResource.java
@@ -37,8 +37,8 @@ import static java.util.stream.Collectors.toList;
 @Path("files")
 public class FilesResource {
     private static final Logger LOGGER = LoggerFactory.getLogger(FilesResource.class);
-    private final String CONVERTFILETYPE = "pdf,jpg,jpeg,font,gif,png,wav";
-    private final String imageType="jpg,jpeg,png,gif";
+    private final String CONVERTFILETYPE = "pdf,jpg,jpeg,font,gif,png,wav,pdf,JPG,JPEG,FONT,GIF,PNG,WAV";
+    private final String imageType="jpg,jpeg,png,gif,JPG,JPEG,PNG,GIF";
     private final String BASE_ENCODE = "89601CD4D2A12A979D1E284DE53E3562";//32位随机数
     @POST
     @Consumes(MediaType.MULTIPART_FORM_DATA)
@@ -147,6 +147,7 @@ public class FilesResource {
             }
         } catch (Exception e) {
             e.printStackTrace();
+            LOGGER.info("pdf文件转换失败" + e);
         } finally {
             //文件上传到oss
            fileId = fileService.storeForCredit(sourceName, destName, inputStreamForUpload, source,url);
